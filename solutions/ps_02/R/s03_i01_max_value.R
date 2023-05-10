fceecc69e3eb261a33081985b614a574560aa43f
ShaineRosewel <shainematala@gmail.com>
2023-05-11 01:51:43 +0800

    answered ps2

--- solutions/ps_02/R/s03_i01_max_value.R
+++
@@ -1,9 +0,0 @@
-    source("../R/s02_i01_bs_sampling.R")
-    
-    r_max <- max(r)
-    
-    r_replaced <- replace(r, r==r_max, 100*r_max)
-    r_replaced_max <- max(r_replaced)
-    se_r_replaced_boot <- sd(r_replaced)
-    
-    se_percent_change <- ((se_r_replaced_boot - se_r_boot)/se_r_boot)*100
